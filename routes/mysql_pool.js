80311c8f70577187765361a690941272c5dbeb33
mu77 <Zhaoqinglong@Mu77.com>
2017-08-04 13:25:29 +0800

    更新

--- routes/mysql_pool.js
+++ routes/mysql_pool.js
@@ -42,35 +42,6 @@ exports.RegisterAccount = function RegisterAccount(name, password,group_id,func)
     });
 }
 
-exports.GetOrderState = function(trade_no, func){
-    var sql_query = 'select * from orders_success where trade_no = \'' + trade_no + '\'';
-    mysql_pool.query(sql_query, func);
-}
-
-exports.OrderSuccess = function(trade_no, platform_order,user_id,account_platfrom,account,login_ip,product_id,total_fee,server_id,device_id){   
-    let querystr = "insert into orders_success \
-                    (trade_no, platform_order, user_id, \
-                    account_platfrom, account, login_ip, product_id, \
-                    total_fee, server_id, device_id, create_time) \
-                    value('%s', '%s', '%s', '%s', '%s', '%s', '%s', %d, %d, '%s', now())";
-    querystr = util.format(querystr,trade_no, platform_order,user_id,account_platfrom,account,login_ip,product_id,total_fee,server_id,device_id);
-
-    mysql_pool.query(querystr, function(err, rows, fields) {
-        if(err) console.error(err);
-    });
-}
-
-exports.OrderFail = function(user_id, trade_no, platform_order, errcode,product_id, total_fee,server_id, device_id, detail){
-    let querystr = "insert into orders_fail \
-                    (user_id, trade_no, platform_order, errcode, \
-                    product_id, total_fee, \
-                    server_id, device_id, detail,create_time) \
-                    value('%s', '%s', '%s', %d, '%s', %d, %d, '%s', '%s', now())";
-    var sql_query = util.format(querystr,user_id, trade_no, platform_order, errcode,product_id, total_fee,server_id, device_id, detail);                  
-    mysql_pool.query(sql_query, function(err, rows, fields) {
-        if(err) console.error(err);
-    });
-}
 
 
 
